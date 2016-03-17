8ae83f5ca8d9089bf16ccb72a49db6f8d325415c
Pedro Rodrigues Lima <perliima72@gmail.com>
2016-03-16 21:34:51 -0300

    subjects features completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160316024357) do
+ActiveRecord::Schema.define(version: 20160316032454) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20160316024357) do
     t.integer  "credit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "semester"
   end
 
   create_table "users", force: :cascade do |t|
